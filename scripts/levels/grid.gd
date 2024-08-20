d1879c52f4088a4f34e69dee5892c753e31191ee
Richard Ortiz <SlowFaller@users.noreply.github.com>
2024-08-19 17:03:21 -0700

    Update grid.gd

--- scripts/levels/grid.gd
+++ scripts/levels/grid.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
+@export var grid_position: Vector2 = Vector2.ZERO
+@export var 
+
 var cell_size: int = 32 # cells are square
 var grid_size: Array[int] = [4, 4]
 var mouse_position_in_world: Vector2i = Vector2i.ZERO
-var grid_position: Vector2i = Vector2i.ZERO
 var selected_cell: Vector2i = Vector2i.ZERO
 var highlighted_cell: Rect2i = Rect2i(320, 180, 32, 32)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$PlacedTileMap.position = grid_position
 	_calculate_grid()
 	
 
