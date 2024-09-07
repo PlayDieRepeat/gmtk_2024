c04afb7e359bdd9ffc53ac574a772699b046f644
Richard Ortiz <SlowFaller@users.noreply.github.com>
2024-09-06 19:23:05 -0700

    Fixed some bugs
    The game is relatively bug free.

--- scripts/systems/world_controller.gd
+++ scripts/systems/world_controller.gd
@@ -14,28 +14,28 @@ signal new_hour(p_new_hour: int, p_old_hour: int, p_time_constants: RTimeConstan
 signal set_god(p_god_data: RGod)
 
 func _ready():
-	_load_all_resources()
-	ui_time = $CanvasLayer/MarginContainer/VBoxContainer/Time
-	ui_tithing = $CanvasLayer/MarginContainer/VBoxContainer/Tithing
-	ui_tithing.text = "Tithing: "
-	ui_favor = $CanvasLayer/MarginContainer/VBoxContainer/Favor
-	ui_favor.text = "Favor: "
-	var god_background: TextureRect = $ParallaxBackground/GodLayer/GodImage
-	var new_god: RGod = ResourceLoader.load("res://resources/gods/cat_god.tres", "RGod")
-	%God.set_god(new_god)
-	god_background.texture = %God.god_data.image
-	hour_count = time_constant.start_of_day
-	$ParallaxBackground/AlphaLayer/AlphaImage.self_modulate.a = alpha_float
-	var margin_value: int = 15
-	$CanvasLayer/MarginContainer.add_theme_constant_override("margin_left", margin_value)
-	alpha_change_float = alpha_float/(time_constant.seconds_in_hour*6)
+	#_load_all_resources()
+	#ui_time = $CanvasLayer/MarginContainer/VBoxContainer/Time
+	#ui_tithing = $CanvasLayer/MarginContainer/VBoxContainer/Tithing
+	#ui_tithing.text = "Tithing: "
+	#ui_favor = $CanvasLayer/MarginContainer/VBoxContainer/Favor
+	#ui_favor.text = "Favor: "
+	#var god_background: TextureRect = $ParallaxBackground/GodLayer/GodImage
+	#var new_god: RGod = ResourceLoader.load("res://resources/gods/cat_god.tres", "RGod")
+	#%God.set_god(new_god)
+	#god_background.texture = %God.god_data.image
+	#hour_count = time_constant.start_of_day
+	#$ParallaxBackground/AlphaLayer/AlphaImage.self_modulate.a = alpha_float
+	#var margin_value: int = 15
+	#$CanvasLayer/MarginContainer.add_theme_constant_override("margin_left", margin_value)
+	#alpha_change_float = alpha_float/(time_constant.seconds_in_hour*6)
 	# Required resources should be asserted at ready to stop us from doing bad things
 	assert(time_constants != null, "Time Constants not set in WorldController")
 	assert(god_data != null, "No default God Data set in WorldController")
 
 	# Store reference to world_timer and connect signal
 	# TODO - Should we just instantiate the Timer here as a node? think it depends how we handle time going forward.
-	world_timer = get_tree().get_first_node_in_group("world_timer")
+	world_timer = %GlobalTimer
 	world_timer.timeout.connect(_on_timer_timeout)
 
 	hour_count = time_constants.start_of_day
