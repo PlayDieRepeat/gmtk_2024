9b633ad0a9e0fb3fd192bbb28881d44e09ade401
Richard Ortiz <SlowFaller@users.noreply.github.com>
2025-02-12 03:53:03 -0800

    tracking mouse on terrain

--- scenes/3d/main_test_3d.gd
+++ scenes/3d/main_test_3d.gd
@@ -7,6 +7,7 @@ var heightmap: Texture2D = null
 var shader_mat: ShaderMaterial = null
 var shader_mat_is_set := false
 var height_offset_vertex_scalar := 0.0
+var static_body : StaticBody3D = null
 
 var capture_mouse := true
 
@@ -24,6 +25,13 @@ func _ready() -> void:
 		shader_mat_is_set = true
 		
 	terrain_mesh.create_convex_collision(true)
+	var temp_node = terrain_mesh.get_child(0)
+	if temp_node != null:
+		if temp_node is StaticBody3D:
+			static_body = temp_node as StaticBody3D
+			static_body.collision_mask = 2
+			static_body.input_event.connect(on_input_event)
+			print("Static body signal hooked up.")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("build"):
@@ -42,3 +50,6 @@ func _physics_process(_delta: float) -> void:
 		height_offset_vertex_scalar -= 0.5
 		if shader_mat_is_set:
 			shader_mat.set_shader_parameter('uHeight_offset', height_offset_vertex_scalar)
+
+func on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	print("I am clicked on at: ", event_position)
